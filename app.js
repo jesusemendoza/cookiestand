2d07489f0d99a573d634b6b560ab28944a62c090
jesus <je.mendoza@live.com>
2017-11-13 18:18:34 -0800

    Pikes place items are now loading onto sales scree

--- app.js
+++ app.js
@@ -6,5 +6,25 @@ var pike = {
   avg: 6.3,
   randomCustomer: function () {
     return Math.random() * (this.max-this.min)+(this.min);
+  },
+  cookiesale: function() {
+    return Math.round(this.randomCustomer() * this.avg);
+  },
+  saleprintout: function (){
+    for (var i = 0; i<15; i++) {
+      var arrayHours = ['6am','7am','8am','9am','10am','11am','12am','1pm','2pm','3pm','4pm','5pm','6pm','7pm','8pm'];
+      // create a new div element
+      // and give it some content
+      var newLi = document.createElement('li');
+      var newContent = document.createTextNode(arrayHours[i] + ': ' + this.cookiesale() + ' cookies');
+      newLi.appendChild(newContent); //add the text node to the newly created div.
+
+      // add the newly created element and its content into the DOM
+      var currentLi = document.getElementById('li1');
+      document.body.insertBefore(newLi, currentLi);
+    }
   }
 };
+
+pike.saleprintout();
+//var arrayHours = ['6am','7am','8am','9am','10am','11am','12am','1pm','2pm','3pm','4pm','5pm','6pm','7pm','8pm'];
